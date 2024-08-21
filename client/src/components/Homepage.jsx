a28000438a00553c2d599ab5710f04e83b684dcb
bedlam343 <jagik22@gmail.com>
2024-08-21 16:34:34 -0700

    design for smaller screens

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,22 +1,42 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import drawTriangle from 'components/utils/drawTriangle';
+import clearTriangle from './utils/clearTriangle';
 
 const Homepage = ({ bootcamps }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const triangleSize1Ref = useRef();
   const triangleSize2Ref = useRef();
   const triangleSize3Ref = useRef();
   const triangleSize4Ref = useRef();
 
+  // update state when window size changes
+  useEffect(() => {
+    const updateWindowWith = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWith);
+
+    return () => window.removeEventListener('resize', updateWindowWith);
+  }, []);
+
   // draw 4 upside-down triangles
   useEffect(() => {
+    if (windowWidth < 640) {
+      // delete triangles if screen width is small
+      clearTriangle(triangleSize1Ref);
+      clearTriangle(triangleSize2Ref);
+      clearTriangle(triangleSize3Ref);
+      clearTriangle(triangleSize4Ref);
+      return;
+    }
+
     const drawTriangles = () => {
-      const height = window.innerHeight;
-      const halfway = height / 1.75;
-      drawTriangle(triangleSize1Ref, halfway, '#1E1E1E');
-      drawTriangle(triangleSize2Ref, halfway + 20, '#595959');
-      drawTriangle(triangleSize3Ref, halfway + 40, '#969696');
-      drawTriangle(triangleSize4Ref, halfway + 60, '#D6D5D5');
+      drawTriangle(triangleSize1Ref, 500, '#1E1E1E');
+      drawTriangle(triangleSize2Ref, 520, '#595959');
+      drawTriangle(triangleSize3Ref, 540, '#969696');
+      drawTriangle(triangleSize4Ref, 560, '#D6D5D5');
     };
 
     drawTriangles();
@@ -24,87 +44,125 @@ const Homepage = ({ bootcamps }) => {
     window.addEventListener('resize', drawTriangles);
 
     return () => window.removeEventListener('resize', drawTriangles);
-  }, []);
+  }, [windowWidth]);
 
   return (
     <>
       <div className="bg-easyWhite pb-14">
         {/* Upside-down triangles */}
-        <div className="">
+        <div className="border-b-2 border-gray-300 sm:border-none">
           <canvas ref={triangleSize4Ref} className="block absolute" />
           <canvas ref={triangleSize3Ref} className="block absolute" />
           <canvas ref={triangleSize2Ref} className="block absolute" />
           <canvas ref={triangleSize1Ref} className="block absolute" />
 
           {/* Title and Hammer */}
-          <div className="absolute w-full flex flex-col items-center gap-8 pt-4">
-            <div className="flex flex-col items-center">
-              <p className="text-easyWhite text-[80px]">Become a Digital</p>
-              <p className="text-easyWhite text-[80px]">Craftsman.</p>
+          <div className="sm:absolute sm:w-full flex flex-col items-center gap-8 pt-4 pb-12 sm:pb-0">
+            <div className="flex flex-col items-center px-6 sm:px-0">
+              <p className="text-lightBlack sm:text-easyWhite text-[50px] sm:text-[80px] text-center">
+                Become a Digital
+              </p>
+              <p className="text-lightBlack sm:text-easyWhite text-[50px] sm:text-[80px] text-center">
+                Craftsman.
+              </p>
             </div>
             <img
               src="./assets/hammer.png"
               alt="Codemasons Logo"
-              className="w-[130px] animate-hammer-swing"
+              className="w-[120px] sm:w-[130px] animate-hammer-swing"
             />
           </div>
         </div>
 
         {/* Reviews */}
-        <div
-          className="flex"
-          style={{ height: window.innerHeight / 1.75 + 60 }}
-        >
-          <div className="w-[50%] flex items-center flex-col">
-            <div className="h-[50%] w-[100%]" />
+        {windowWidth >= 640 ? (
+          <>
+            <div className="flex sm:h-[560px]">
+              <div className="w-[50%] flex items-center flex-col">
+                <div className="h-[50%] w-[100%]" />
 
-            <div className="h-[50%] w-[100%] flex">
-              <div className="w-[60%] flex flex-col items-center px-8 gap-4">
-                <img
-                  src="./assets/reviewer1.jpg"
-                  alt="Reviewier 1"
-                  className="object-cover h-[125px] w-[125px] rounded-full"
-                />
-                <p className="text-center font-inter text-[14px]">
-                  "Codemasons gave me the secrets to unlock my greatest and
-                  hidden potential."
-                </p>
-                <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
-                  Jagjit Singh - 3rd Degree Codemason
-                </p>
+                <div className="h-[50%] w-[100%] flex">
+                  <div className="w-[60%] flex flex-col items-center px-8 gap-4">
+                    <img
+                      src="./assets/reviewer1.jpg"
+                      alt="Reviewier 1"
+                      className="object-cover h-[125px] w-[125px] rounded-full"
+                    />
+                    <p className="text-center font-inter text-[14px]">
+                      "Codemasons gave me the secrets to unlock my greatest and
+                      hidden potential."
+                    </p>
+                    <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
+                      Jagjit Singh - 3rd Degree Codemason
+                    </p>
+                  </div>
+
+                  <div className="w-[40%]" />
+                </div>
               </div>
 
-              <div className="w-[40%]" />
-            </div>
-          </div>
+              <div className="w-[50%] flex items-center flex-col">
+                <div className="h-[50%] w-[100%]" />
 
-          <div className="w-[50%] flex items-center flex-col">
-            <div className="h-[50%] w-[100%]" />
-
-            <div className="h-[50%] w-[100%] flex">
-              <div className="w-[40%]" />
-              <div className="w-[60%] flex flex-col items-center pl-9 pr-12 gap-4">
-                <img
-                  src="./assets/reviewer2.png"
-                  alt="Reviewier 1"
-                  className="object-cover h-[125px] w-[125px] rounded-full"
-                />
-                <p className="text-center font-inter text-[14px]">
-                  "I had almost given up on coding, and then I discovered
-                  Codemasons. Now, I am a code-magician. I swear, these people
-                  practice witchcraft."
-                </p>
-                <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
-                  Anonymous
-                </p>
+                <div className="h-[50%] w-[100%] flex">
+                  <div className="w-[40%]" />
+                  <div className="w-[60%] flex flex-col items-center pl-9 pr-12 gap-4">
+                    <img
+                      src="./assets/reviewer2.png"
+                      alt="Reviewier 1"
+                      className="object-cover h-[125px] w-[125px] rounded-full"
+                    />
+                    <p className="text-center font-inter text-[14px]">
+                      "I had almost given up on coding, and then I discovered
+                      Codemasons. Now, I am a code-magician. I swear, these
+                      people practice witchcraft."
+                    </p>
+                    <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
+                      Anonymous
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <div className="flex flex-col py-10 pb-4 gap-8">
+            <div className="flex flex-col items-center px-8 gap-4">
+              <img
+                src="./assets/reviewer1.jpg"
+                alt="Reviewier 1"
+                className="object-cover h-[125px] w-[125px] rounded-full"
+              />
+              <p className="text-center font-inter text-[14px] text-lightBlack">
+                "Codemasons gave me the secrets to unlock my greatest and hidden
+                potential."
+              </p>
+              <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
+                Jagjit Singh - 3rd Degree Codemason
+              </p>
+            </div>
 
-        <div className="mt-14 flex justify-center">
-          <div className="w-[45%]">
-            <p className="font-inter text-center text-[36px]">
+            <div className="flex flex-col items-center px-8 gap-6">
+              <img
+                src="./assets/reviewer2.png"
+                alt="Reviewier 1"
+                className="object-cover h-[125px] w-[125px] rounded-full"
+              />
+              <p className="text-center font-inter text-[14px] text-lightBlack">
+                "I had almost given up on coding, and then I discovered
+                Codemasons. Now, I am a code-magician. I swear, these people
+                practice witchcraft."
+              </p>
+              <p className="text-center font-inter uppercase text-[#3D3D3D] text-[12px]">
+                Anonymous
+              </p>
+            </div>
+          </div>
+        )}
+
+        <div className="pt-10 px-6 sm:px-0 sm:mt-14 sm:pt-0 flex justify-center">
+          <div className="sm:w-[45%]">
+            <p className="font-inter text-center text-[24px] sm:text-[36px] font-medium italic">
               We don't just create programmers, we create{' '}
               <span className="text-themeOrange">digital craftsmen</span>.
             </p>
--- client/src/components/MainNavigation.jsx
+++ client/src/components/MainNavigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import { NavLink, useSubmit } from 'react-router-dom';
 
@@ -6,8 +6,20 @@ import BootcampsDropdown from 'components/Bootcamp/Dropdown';
 import { useAuth } from 'store/AuthProvider';
 
 const MainNavigation = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const { isLoggedIn } = useAuth();
   const submit = useSubmit();
+
+  // update state when window size changes
+  useEffect(() => {
+    const updateWindowWith = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWith);
+
+    return () => window.removeEventListener('resize', updateWindowWith);
+  }, []);
 
   const handleLogout = () => {
     submit({}, { method: 'POST' });
@@ -31,23 +43,40 @@ const MainNavigation = () => {
   };
 
   return (
-    <div className="w-full py-4 bg-[#000000] flex items-center justify-evenly">
+    <div className="w-full py-3 pl-6 pr-10 sm:px-0 sm:py-4 bg-[#000000] flex items-center justify-between sm:justify-evenly">
       <div className="flex gap-2 items-center justify-center hover:cursor-pointer">
         <img
           src="./assets/hammer.png"
           alt="Codemason Logo"
-          className="w-[55px]"
+          className="w-[40px] sm:w-[55px]"
         />
-        <p className="text-themeBlue text-4xl font-kellySlab">Codemasons</p>
+        <p className="text-themeBlue text-2xl sm:text-4xl font-kellySlab">
+          Codemasons
+        </p>
       </div>
-      <ul className="text-easyWhite font-cairo font-semibold flex justify-evenly text-md uppercase min-w-[600px]">
-        <li className="hover:cursor-pointer hover:text-themeBlue">Programs</li>
-        <li className="hover:cursor-pointer hover:text-themeBlue">Teach</li>
-        <li className="hover:cursor-pointer hover:text-themeBlue">About</li>
-      </ul>
-      <button className="rounded-lg w-[120px] h-[50px] bg-themeOrange hover:bg-themeBlue flex items-center justify-center">
-        <p className="text-white uppercase">Sign Up</p>
-      </button>
+
+      {windowWidth >= 640 ? (
+        // List of Links
+        <>
+          <ul className="text-easyWhite font-cairo font-semibold flex justify-evenly text-md uppercase min-w-[600px]">
+            <li className="hover:cursor-pointer hover:text-themeBlue">
+              Programs
+            </li>
+            <li className="hover:cursor-pointer hover:text-themeBlue">Teach</li>
+            <li className="hover:cursor-pointer hover:text-themeBlue">About</li>
+          </ul>
+          <button className="rounded-lg w-[120px] h-[50px] bg-themeOrange hover:bg-themeBlue flex items-center justify-center">
+            <p className="text-white uppercase">Sign Up</p>
+          </button>
+        </>
+      ) : (
+        // Dropdown
+        <div className="flex flex-col gap-1 hover:cursor-pointer">
+          <div className="w-[20px] h-[2px] bg-white" />
+          <div className="w-[20px] h-[2px] bg-white" />
+          <div className="w-[20px] h-[2px] bg-white" />
+        </div>
+      )}
     </div>
   );
 };
