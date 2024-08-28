33aafbd9313190e5cf7b541f088d1403e22a7217
bedlam343 <jagik22@gmail.com>
2024-08-28 16:08:20 -0700

    ProgramForm component

---
+++ client/src/components/Program/ProgramForm.jsx
@@ -0,0 +1,242 @@
+import Checkbox from 'components/ui/Checkbox';
+import Stickers from 'components/ui/Stickers';
+import TextField from 'components/ui/TextField';
+import { useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+const initialErrors = {};
+
+const ProgramForm = ({ onSubmit }) => {
+  const [errors, setErrors] = useState({
+    ...initialErrors,
+  });
+
+  const [uploadedPhoto, setUploadedPhoto] = useState(null);
+  const [previewImage, setPreviewImage] = useState(false);
+  const [previewUrl, setPreviewUrl] = useState('');
+
+  const [careers, setCareers] = useState([]);
+
+  const nameRef = useRef();
+  const descriptionRef = useRef();
+  const careersRef = useRef();
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith('image/')) {
+      setUploadedPhoto(file);
+      if (errors.photoError) setErrors((errs) => ({ ...errs, photoError: '' }));
+    } else {
+      setErrors((errs) => ({ ...errs, photoError: 'Invalid file type.' }));
+    }
+  };
+
+  const onPreviewImage = () => {
+    setPreviewUrl(URL.createObjectURL(uploadedPhoto));
+    setPreviewImage(true);
+  };
+
+  const renderImagePreview = () => {
+    if (!previewImage || !previewUrl) return false;
+    return createPortal(
+      <div
+        onMIsse
+        className="fixed w-[100%] h-[100%] top-0 z-50 left-0 bg-black 
+      flex items-center justify-center bg-opacity-90"
+      >
+        <div className="w-[500px] flex flex-col items-center gap-10 h-auto">
+          <div
+            className="bold text-white text-2xl hover:cursor-pointer hover:underline"
+            onClick={onExitPreview}
+          >
+            Exit Preview
+          </div>
+          <img src={previewUrl} alt="Selected" />
+        </div>
+      </div>,
+      document.getElementById('overlays')
+    );
+  };
+
+  const onExitPreview = () => {
+    URL.revokeObjectURL(previewUrl);
+    setPreviewImage(false);
+    setPreviewUrl('');
+  };
+
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+
+    if (!nameRef.current.value) {
+      nameRef.current.focus();
+      setErrors((errs) => ({ ...errs, nameEmptyError: true }));
+      return;
+    }
+
+    if (!descriptionRef.current.value) {
+      descriptionRef.current.focus();
+      setErrors((errs) => ({ ...errs, descriptionEmptyError: true }));
+      return;
+    }
+
+    if (careers.length === 0) {
+      careersRef.current.focus();
+      setErrors((errs) => ({ ...errs, careersEmptyError: true }));
+      return;
+    }
+
+    if (!uploadedPhoto) {
+      setErrors((errs) => ({ ...errs, photoError: 'Please Upload an Image.' }));
+      return;
+    }
+
+    const formData = new FormData(event.currentTarget);
+    onSubmit(formData);
+  };
+
+  return (
+    <>
+      <form
+        onChange={() => setErrors({ ...initialErrors })}
+        onSubmit={handleFormSubmission}
+        className="flex flex-col items-start mt-8 gap-6"
+      >
+        <TextField
+          id="name"
+          label="Program Name"
+          ref={nameRef}
+          width={300}
+          errorFill={errors.nameEmptyError}
+          required
+        />
+
+        <div className="w-[100%]">
+          <label
+            htmlFor="description"
+            className="text-white block font-inter text-[16px]"
+          >
+            Description <span className="text-red-400">*</span>
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            ref={descriptionRef}
+            rows={5}
+            className={`font-cairo outline-none mt-1 px-2 py-2 w-[100%] text-sm bg-easyWhite
+      ${errors.descriptionEmptyError ? 'focus:bg-red-500' : 'focus:bg-white'}`}
+          />
+        </div>
+
+        <div className="flex justify-between items-end w-[100%]">
+          <TextField id="address" label="Address" width={300} />
+          <button
+            type="button"
+            className="text-white border-white border-2 rounded-[5px] 
+    w-[80px] h-[40px] ease-in-out duration-300 hover:border-themeBlue hover:text-themeBlue"
+          >
+            Locate
+          </button>
+        </div>
+
+        <div className="w-[100%]">
+          <label
+            htmlFor="careers"
+            className="text-white block font-inter text-[16px]"
+          >
+            Tags (1-3) <span className="text-red-400">*</span>
+          </label>
+          <input
+            className="hidden"
+            name="careers"
+            id="careers"
+            value={careers}
+          />
+          <Stickers
+            items={careers}
+            ref={careersRef}
+            error={errors.careersEmptyError}
+            maxItems={3}
+            onAddItem={(career) => {
+              setCareers((currCareers) => [...currCareers, career]);
+              setErrors((errs) => ({ ...errs, careersEmptyError: false }));
+            }}
+            onRemoveItem={(careerToRemove) =>
+              setCareers((currCareers) => {
+                return currCareers.filter(
+                  (career) => career !== careerToRemove
+                );
+              })
+            }
+          />
+        </div>
+
+        <div className="w-[100%]">
+          <label className="text-white block font-inter text-[16px]">
+            Benefits
+          </label>
+          <div className="mt-1 flex justify-between w-[100%] gap-8">
+            <Checkbox id="housing" label="Housing" gap="gap-3" />
+            <Checkbox id="jobGuarantee" label="Job Guarantee" gap="gap-3" />
+            <Checkbox id="jobAssistance" label="Job Assistance" gap="gap-3" />
+          </div>
+        </div>
+
+        <div>
+          <label className="text-white block font-inter text-[16px]">
+            Image <span className="text-red-400">*</span>
+          </label>
+          <div className="flex gap-4 mt-1 items-center">
+            <input
+              id="photo"
+              name="photo"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
+            />
+
+            <label
+              className="text-white text-[14px] border-white border-2 rounded-[5px] 
+      w-[85px] h-[35px] ease-in-out duration-300 hover:border-themeBlue
+     hover:text-themeBlue hover:cursor-pointer flex items-center justify-center"
+              htmlFor="photo"
+            >
+              {!uploadedPhoto ? 'Upload' : 'Re-Upload'}
+            </label>
+
+            {!uploadedPhoto ? (
+              <p className="text-[#959595] text-[14px] italic font-cairo">
+                No Image Uploaded.
+              </p>
+            ) : (
+              <div
+                onClick={onPreviewImage}
+                className="text-white text-[15px] hover:underline 
+        hover:cursor-pointer font-cairo"
+              >
+                {uploadedPhoto.type}
+              </div>
+            )}
+          </div>
+          {errors.photoError && (
+            <p className="text-red-400 mt-1 font-cairo">{errors.photoError}</p>
+          )}
+        </div>
+
+        <div className={`flex w-[100%] mt-12 justify-end`}>
+          <button
+            type="submit"
+            className={`min-w-[90px] bg-easyWhite hover:bg-white w-fit h-[40px] py-2 px-4 flex justify-center
+    items-center rounded-[5px] font-cairo ease-linear duration-300`}
+          >
+            Next
+          </button>
+        </div>
+      </form>
+
+      {renderImagePreview()}
+    </>
+  );
+};
+
+export default ProgramForm;
--- client/src/components/ui/Stickers.jsx
+++ client/src/components/ui/Stickers.jsx
@@ -1,56 +1,72 @@
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
-const Stickers = ({ items, onAddItem, onRemoveItem, maxItems }) => {
-  const [careerInputValue, setCareerInputValue] = useState('');
+const Stickers = forwardRef(
+  ({ items, onAddItem, onRemoveItem, maxItems, error }, ref) => {
+    const [careerInputValue, setCareerInputValue] = useState('');
+    const [focus, setFocus] = useState(false);
 
-  const handleCareerInputKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      if (!event.target.value) return;
-      if (items.includes(event.target.value)) {
+    const bgColor =
+      error && focus ? 'bg-red-500' : focus ? 'bg-white' : 'bg-easyWhite';
+
+    const handleCareerInputKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        if (!event.target.value) return;
+        if (items.includes(event.target.value)) {
+          setCareerInputValue('');
+          return;
+        }
+
+        onAddItem(event.target.value);
         setCareerInputValue('');
-        return;
       }
+    };
 
-      onAddItem(event.target.value);
-      setCareerInputValue('');
-    }
-  };
+    const handleInputChange = (event) => {
+      setCareerInputValue(event.target.value);
+    };
 
-  const handleInputChange = (event) => {
-    setCareerInputValue(event.target.value);
-  };
-
-  return (
-    <div className="flex justify-start gap-x-4 gap-y-1 w-[100%] bg-white items-center px-2 py-2 flex-wrap mt-1">
-      {items.map((item) => (
-        <div
-          key={item}
-          className="bg-gray-400 px-4 py-2 rounded-[5px] h-[40px] 
+    return (
+      <div
+        className={`group flex justify-start gap-x-4 gap-y-1 w-[100%] 
+      ${bgColor}  items-center px-2 py-2 
+      flex-wrap mt-1`}
+      >
+        {items.map((item) => (
+          <div
+            key={item}
+            className="bg-gray-400 px-4 py-2 rounded-[5px] h-[40px] 
       flex gap-3 items-center justify-between"
-        >
-          <p className="text-white"> {item}</p>
-          <p
-            className="font-bold font-cairo hover:cursor-pointer"
-            onClick={() => onRemoveItem(item)}
           >
-            X
-          </p>
-        </div>
-      ))}
-      {items.length < maxItems && (
-        <input
-          type="text"
-          onChange={handleInputChange}
-          placeholder={items.length < maxItems ? 'Type and Press Enter' : ''}
-          onKeyDown={handleCareerInputKeyDown}
-          disabled={items.length === maxItems}
-          value={careerInputValue}
-          className="h-[50px] outline-none px-2 font-cairo text-sm"
-        />
-      )}
-    </div>
-  );
-};
+            <p className="text-white"> {item}</p>
+            <p
+              className="font-bold font-cairo hover:cursor-pointer"
+              onClick={() => onRemoveItem(item)}
+            >
+              X
+            </p>
+          </div>
+        ))}
+        {items.length < maxItems && (
+          <input
+            type="text"
+            ref={ref}
+            onChange={handleInputChange}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            placeholder={items.length < maxItems ? 'Type and Press Enter' : ''}
+            onKeyDown={handleCareerInputKeyDown}
+            disabled={items.length === maxItems}
+            value={careerInputValue}
+            className={`h-[50px] outline-none px-2 font-cairo text-sm 
+            bg-easyWhite ${
+              error ? 'focus:bg-red-500 placeholder-white' : 'focus:bg-white'
+            }`}
+          />
+        )}
+      </div>
+    );
+  }
+);
 
 export default Stickers;
