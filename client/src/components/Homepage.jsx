6a998a640d64b9a653fd6b4b36b4706aa77873d7
bedlam343 <jagik22@gmail.com>
2024-09-03 11:05:12 -0700

    give homepage and main navigation a max height

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -3,105 +3,107 @@ import { NavLink } from 'react-router-dom';
 const Homepage = ({ bootcamps }) => {
   return (
     <>
-      <div className="bg-easyWhite pb-14 grid grid-cols-1">
-        {/* title and hammer */}
-        <div
-          className="top-[70px] w-[100%] sm:hidden border-b-2 border-[#e1e1e1] 
-            flex flex-col items-center gap-8 pt-4 sm:pt-12 pb-12"
-        >
-          <div className="flex flex-col items-center px-8">
-            <p className="text-lightBlack text-[50px] text-center">
-              Become a Digital
-            </p>
-            <p className="text-lightBlack  text-[50px] text-center">
-              Craftsman.
-            </p>
-          </div>
-          <img
-            src="./assets/hammer.png"
-            alt="Codemasons Logo"
-            className="w-[100px] sm:w-[0px] animate-hammer-swing sm:animate-none"
-          />
-        </div>
-
-        <div
-          className="flex flex-col justify-center sm:flex-row h-auto pt-4 pb-10 sm:pt-0 
-          sm:mt-[0px] gap-12 sm:gap-0 border-b-2 border-[#e1e1e1] sm:border-none"
-        >
-          <div className="flex flex-col items-center px-8 gap-4 justify-end w-[100%] sm:w-[30%]">
-            <img
-              src="./assets/reviewer1.jpg"
-              alt="Reviewier 1"
-              className="object-cover h-[125px] w-[125px] rounded-full"
-            />
-            <p
-              className="text-center font-inter text-[14px] sm:text-[12px] 
-                      md:text-[13px] lg:text-[14px]"
-            >
-              "Codemasons gave me the secrets to unlock my greatest and hidden
-              potential."
-            </p>
-            <p
-              className="text-center font-inter uppercase text-[#3D3D3D] 
-                      text-[12px] sm:text-[10px] md:text-[11px] lg:text-[12px]"
-            >
-              Jagjit Singh - 3rd Degree Codemason
-            </p>
-          </div>
-
+      <div className="bg-easyWhite pb-14 flex justify-center">
+        <div className="flex flex-col lg:max-w-[1920px]">
           {/* title and hammer */}
           <div
-            className="hidden sm:w-auto sm:relative sm:flex flex-col 
-            items-center gap-8 sm:gap-16 md:gap-12 lg:gap-8 pt-4 sm:pt-12 
-            md:pt-8 lg:pt-4 pb-12 sm:pb-0"
+            className="top-[70px] w-[100%] sm:hidden border-b-2 border-[#e1e1e1] 
+            flex flex-col items-center gap-8 pt-4 sm:pt-12 pb-12"
           >
-            <div className="flex flex-col items-center px-6 sm:px-0">
-              <p className="text-lightBlack  text-[50px] md:text-[60px] lg:text-[80px] text-center">
+            <div className="flex flex-col items-center px-8">
+              <p className="text-lightBlack text-[50px] text-center">
                 Become a Digital
               </p>
-              <p className="text-lightBlack  text-[50px] md:text-[60px] lg:text-[80px] text-center">
+              <p className="text-lightBlack  text-[50px] text-center">
                 Craftsman.
               </p>
             </div>
             <img
               src="./assets/hammer.png"
               alt="Codemasons Logo"
-              className="w-[100px] md:w-[115px] lg:w-[130px] animate-hammer-swing"
+              className="w-[100px] sm:w-[0px] animate-hammer-swing sm:animate-none"
             />
           </div>
 
           <div
-            className="flex flex-col items-center pl-9 pr-12 gap-4 
-            justify-end w-[100%] sm:w-[30%]"
+            className="flex flex-col justify-center sm:flex-row h-auto pt-4 pb-10 sm:pt-0 
+          sm:mt-[0px] gap-12 sm:gap-0 border-b-2 border-[#e1e1e1] sm:border-none"
           >
-            <img
-              src="./assets/reviewer2.png"
-              alt="Reviewier 1"
-              className="object-cover h-[125px] w-[125px] rounded-full"
-            />
-            <p
-              className="text-center font-inter text-[14px] sm:text-[12px] 
+            <div className="flex flex-col items-center px-8 gap-4 justify-end w-[100%] sm:w-[30%]">
+              <img
+                src="./assets/reviewer1.jpg"
+                alt="Reviewier 1"
+                className="object-cover h-[125px] w-[125px] rounded-full"
+              />
+              <p
+                className="text-center font-inter text-[14px] sm:text-[12px] 
                       md:text-[13px] lg:text-[14px]"
-            >
-              "I had almost given up on coding, and then I discovered
-              Codemasons. Now, I am a code-magician. I swear, these people
-              practice witchcraft."
-            </p>
-            <p
-              className="text-center font-inter uppercase text-[#3D3D3D] 
+              >
+                "Codemasons gave me the secrets to unlock my greatest and hidden
+                potential."
+              </p>
+              <p
+                className="text-center font-inter uppercase text-[#3D3D3D] 
                       text-[12px] sm:text-[10px] md:text-[11px] lg:text-[12px]"
-            >
-              Anonymous
-            </p>
-          </div>
-        </div>
+              >
+                Jagjit Singh - 3rd Degree Codemason
+              </p>
+            </div>
 
-        <div className="pt-12 sm:pt-10 px-8 sm:px-0 flex justify-center">
-          <div className="sm:w-[45%]">
-            <p className="font-inter text-center text-[24px] md:text-[36px] font-medium italic">
-              We don't just create programmers, we create{' '}
-              <span className="text-themeOrange">digital craftsmen</span>.
-            </p>
+            {/* title and hammer */}
+            <div
+              className="hidden sm:w-auto sm:relative sm:flex flex-col 
+            items-center gap-8 sm:gap-16 md:gap-12 lg:gap-8 pt-4 sm:pt-12 
+            md:pt-8 lg:pt-4 pb-12 sm:pb-0"
+            >
+              <div className="flex flex-col items-center px-6 sm:px-0">
+                <p className="text-lightBlack  text-[50px] md:text-[60px] lg:text-[80px] text-center">
+                  Become a Digital
+                </p>
+                <p className="text-lightBlack  text-[50px] md:text-[60px] lg:text-[80px] text-center">
+                  Craftsman.
+                </p>
+              </div>
+              <img
+                src="./assets/hammer.png"
+                alt="Codemasons Logo"
+                className="w-[100px] md:w-[115px] lg:w-[130px] animate-hammer-swing"
+              />
+            </div>
+
+            <div
+              className="flex flex-col items-center pl-9 pr-12 gap-4 
+            justify-end w-[100%] sm:w-[30%]"
+            >
+              <img
+                src="./assets/reviewer2.png"
+                alt="Reviewier 1"
+                className="object-cover h-[125px] w-[125px] rounded-full"
+              />
+              <p
+                className="text-center font-inter text-[14px] sm:text-[12px] 
+                      md:text-[13px] lg:text-[14px]"
+              >
+                "I had almost given up on coding, and then I discovered
+                Codemasons. Now, I am a code-magician. I swear, these people
+                practice witchcraft."
+              </p>
+              <p
+                className="text-center font-inter uppercase text-[#3D3D3D] 
+                      text-[12px] sm:text-[10px] md:text-[11px] lg:text-[12px]"
+              >
+                Anonymous
+              </p>
+            </div>
+          </div>
+
+          <div className="pt-12 sm:pt-10 px-8 sm:px-0 flex justify-center">
+            <div className="sm:w-[45%]">
+              <p className="font-inter text-center text-[24px] md:text-[36px] font-medium italic">
+                We don't just create programmers, we create{' '}
+                <span className="text-themeOrange">digital craftsmen</span>.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -172,37 +174,6 @@ const Homepage = ({ bootcamps }) => {
                   </div>
                 </div>
               </div>
-              // <div
-              //   key={bootcamp.id}
-              //   className="max-w-[450px] h-auto pb-4 bg-white rounded-[25px] border-2 border-[#8c8c8c]"
-              // >
-              //   <img
-              //     src={bootcamp.photo}
-              //     alt="Bootcamp"
-              //     className="w-[455px] h-[150px] rounded-t-[25px] object-cover"
-              //   />
-              //   <div className="pt-4 px-4">
-              //     <div className="flex justify-between items-end flex-wrap">
-              //       <p className="font-cairo font-semibold text-2xl">
-              //         {bootcamp.name}
-              //       </p>
-              //       <p className="font-cairo text-[#505050] text-[15px]">
-              //         Average Cost: ${bootcamp.averageCost}
-              //       </p>
-              //     </div>
-
-              //     <p className="font-cairo mt-2">{bootcamp.description}</p>
-
-              //     <div className="flex justify-end mt-4">
-              //       <button
-              //         className="rounded-[15px] w-[90px] h-[45px] bg-lightBlack
-              //         hover:bg-[#383838] flex items-center justify-center"
-              //       >
-              //         <p className="text-white uppercase">Details</p>
-              //       </button>
-              //     </div>
-              //   </div>
-              // </div>
             ))}
           </div>
 
--- client/src/components/MainNavigation.jsx
+++ client/src/components/MainNavigation.jsx
@@ -23,67 +23,69 @@ const MainNavigation = () => {
   };
 
   return (
-    <div
-      className="z-50 w-full py-3 pl-6 pr-10 sm:px-0 sm:py-4 bg-[#000000] 
-      flex items-center justify-between sm:justify-evenly border-b-[1px] border-[#575757]"
-    >
-      <NavLink to="/">
-        <div className="flex gap-2 items-center justify-center hover:cursor-pointer">
-          <img
-            src="/assets/hammer.png"
-            alt="Codemason Logo"
-            className="w-[40px] sm:w-[45px] lg:w-[55px]"
-          />
-          <p className="text-themeBlue text-2xl lg:text-4xl font-kellySlab">
-            Codemasons
-          </p>
-        </div>
-      </NavLink>
+    <div className="bg-[#000000] border-[#575757] border-b-[1px] flex justify-center">
+      <div
+        className="z-50 w-full py-3 pl-6 pr-10 sm:px-0 sm:py-4 
+      flex items-center justify-between sm:justify-evenly lg:max-w-[1920px]"
+      >
+        <NavLink to="/">
+          <div className="flex gap-2 items-center justify-center hover:cursor-pointer">
+            <img
+              src="/assets/hammer.png"
+              alt="Codemason Logo"
+              className="w-[40px] sm:w-[45px] lg:w-[55px]"
+            />
+            <p className="text-themeBlue text-2xl lg:text-4xl font-kellySlab">
+              Codemasons
+            </p>
+          </div>
+        </NavLink>
 
-      {windowWidth >= 768 ? (
-        // List of Links
-        <>
-          <ul className="text-easyWhite font-cairo font-semibold flex justify-evenly text-md uppercase min-w-[350px] lg:min-w-[450px] xl:min-w-[600px]">
-            <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
-              <NavLink to="/programs">Learn</NavLink>
-            </li>
-            <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
-              <NavLink to={`/teach/${userId || 'undefined'}`}>Teach</NavLink>
-            </li>
-            <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
-              <NavLink to="/about">About</NavLink>
-            </li>
-          </ul>
+        {windowWidth >= 768 ? (
+          // List of Links
+          <>
+            <ul className="text-easyWhite font-cairo font-semibold flex justify-evenly text-md uppercase min-w-[350px] lg:min-w-[450px] xl:min-w-[600px]">
+              <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
+                <NavLink to="/programs">Learn</NavLink>
+              </li>
+              <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
+                <NavLink to={`/teach/${userId || 'undefined'}`}>Teach</NavLink>
+              </li>
+              <li className="hover:cursor-pointer hover:text-themeBlue text-[14px]  lg:text-[16px]">
+                <NavLink to="/about">About</NavLink>
+              </li>
+            </ul>
 
-          {isLoggedIn ? (
-            <button
-              onClick={handleLogout}
-              className="text-white uppercase rounded-lg w-[90px] lg:w-[120px] 
+            {isLoggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="text-white uppercase rounded-lg w-[90px] lg:w-[120px] 
             h-[45px] lg:h-[50px] bg-themeOrange hover:bg-themeBlue 
             flex items-center justify-center"
-            >
-              Log out
-            </button>
-          ) : (
-            <NavLink to="/login">
-              <button
-                className="text-white uppercase rounded-lg w-[80px] lg:w-[100px] 
+              >
+                Log out
+              </button>
+            ) : (
+              <NavLink to="/login">
+                <button
+                  className="text-white uppercase rounded-lg w-[80px] lg:w-[100px] 
           h-[40px] lg:h-[45px] bg-themeOrange hover:bg-themeBlue 
           flex items-center justify-center"
-              >
-                Join
-              </button>
-            </NavLink>
-          )}
-        </>
-      ) : (
-        // Dropdown
-        <div className="flex flex-col gap-1 hover:cursor-pointer">
-          <div className="w-[20px] h-[2px] bg-white" />
-          <div className="w-[20px] h-[2px] bg-white" />
-          <div className="w-[20px] h-[2px] bg-white" />
-        </div>
-      )}
+                >
+                  Join
+                </button>
+              </NavLink>
+            )}
+          </>
+        ) : (
+          // Dropdown
+          <div className="flex flex-col gap-1 hover:cursor-pointer">
+            <div className="w-[20px] h-[2px] bg-white" />
+            <div className="w-[20px] h-[2px] bg-white" />
+            <div className="w-[20px] h-[2px] bg-white" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
