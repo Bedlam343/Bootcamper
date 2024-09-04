33c377ab29453a1d3395c83ecce585908fee149e
bedlam343 <jagik22@gmail.com>
2024-09-04 10:59:44 -0700

    fix gcp file name name error

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
 
   GCP_BUCKET_NAME: process.env.GCP_BUCKET_NAME,
-  GCP_KEY_FILENAME: process.env.KEY_FILENAME,
+  GCP_KEY_FILENAME: process.env.GCP_KEY_FILENAME,
   GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
 
   GEOCODER_PROVIDER: process.env.GEOCODER_PROVIDER,
